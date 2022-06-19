7d7217da538930df3a0aa1764901e44a9d36186d
wangchunsheng <wangchunsheng04@meituan.com>
2022-06-19 14:12:23 +0800

    add：增加真的swap节点的逻辑

--- src/main/java/com/wang/demo/offer/list/SwapNode.java
+++ src/main/java/com/wang/demo/offer/list/SwapNode.java
@@ -5,6 +5,7 @@ package com.wang.demo.offer.list;
  * @Date: 2022/6/19 10:46 上午
  */
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -33,9 +34,40 @@ public class SwapNode extends AbstractList {
         return head;
     }
 
+    public static ListNode solution1(ListNode head) {
+        if (Objects.isNull(head) || Objects.isNull(head.getNextNode())) {
+            return head;
+        }
+        ListNode newHead = head.getNextNode();
+        ListNode before = null;
+        ListNode cur = head;
+        ListNode next = newHead;
+        while (Objects.nonNull(next)) {
+            swapNeighNode(before, cur, next);
+            before = cur;
+            cur = before.getNextNode();
+            if (Objects.isNull(cur)) {
+                break;
+            }
+            next = cur.getNextNode();
+        }
+
+
+        return newHead;
+    }
+
+    private static void swapNeighNode(ListNode before, ListNode node1, ListNode node2) {
+        ListNode nextNode = node2.getNextNode();
+        if (Objects.nonNull(before)) {
+            before.setNextNode(node2);
+        }
+        node2.setNextNode(node1);
+        node1.setNextNode(nextNode);
+    }
+
     public static void main(String[] args) {
         System.out.println(HEAD);
-        System.out.println(solution(HEAD));
+        System.out.println(solution1(HEAD));
     }
 
 }
