f544fc64170411e105fe0e0b56fdcc7852c490f0
wangchunsheng <wangchunsheng04@meituan.com>
2022-06-22 08:13:53 +0800

    add:查找链表的入环节点

---
+++ src/main/java/com/wang/demo/offer/list/FindTheCircle.java
@@ -0,0 +1,68 @@
+package com.wang.demo.offer.list;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @Author: Wangchunsheng
+ * @Date: 2022/6/22 7:40 上午
+ */
+
+/**
+ * 返回链表的入环节点，如果没有，返回-1
+ */
+public class FindTheCircle {
+
+    public static ListNode solution(ListNode head) {
+        if (Objects.isNull(head)) {
+            return null;
+        }
+        /**
+         * 此处的初始化还是挺重要的，important！！！
+         */
+        ListNode slow = head;
+        ListNode fast = head;
+        while (Objects.nonNull(fast)
+                && Objects.nonNull(fast.getNextNode())) {
+            slow = slow.getNextNode();
+            fast = fast.getNextNode();
+            if (Objects.nonNull(fast)) {
+                fast = fast.getNextNode();
+            }
+            if (slow == fast) {
+                /**
+                 * 说明有环
+                 */
+                ListNode index1 = slow;
+                ListNode index2 = head;
+                while (index1 != index2) {
+                    index1 = index1.getNextNode();
+                    index2 = index2.getNextNode();
+                }
+                return index1;
+            }
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<ListNode> nodeList = Lists.newArrayList();
+        int nodeNumber = 2;
+        for (int i = 0; i < nodeNumber; i++) {
+            nodeList.add(new ListNode(i, null));
+        }
+
+        for (int i = 0; i < nodeNumber - 1; i++) {
+            nodeList.get(i).setNextNode(nodeList.get(i + 1));
+        }
+        ListNode head = nodeList.get(0);
+
+//        nodeList.get(nodeNumber - 1).setNextNode(nodeList.get(0));
+        ListNode solution = solution(head);
+        System.out.println(Objects.nonNull(solution)? solution.getValue(): -1);
+    }
+
+}
