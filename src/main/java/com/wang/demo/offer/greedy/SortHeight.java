81a0b74ef8aece2c32f6590a960d9b60f90dd89b
wangchunsheng <wangchunsheng04@meituan.com>
2022-08-19 09:50:12 +0800

    optimize

--- src/main/java/com/wang/demo/offer/greedy/SortHeight.java
+++ src/main/java/com/wang/demo/offer/greedy/SortHeight.java
@@ -16,22 +16,6 @@ import java.util.*;
 public class SortHeight {
 
     public static List<Pair<Integer, Integer>> solution(List<Pair<Integer, Integer>> people) {
-        Map<Integer, Pair<Integer, Integer>> tmpMap = new HashMap<>();
-        people.sort(SortHeight::comp);
-        for (int i = 0; i < people.size(); i++) {
-            System.out.println(tmpMap);
-            Pair<Integer, Integer> pair = people.get(i);
-            processPairIndex(people.get(i), pair.getValue(), tmpMap);
-        }
-        List<Pair<Integer, Integer>> result = new LinkedList<>();
-        for (int i = 0; i < people.size(); i++) {
-            result.add(tmpMap.get(i));
-        }
-
-        return result;
-    }
-
-    public static List<Pair<Integer, Integer>> solution1(List<Pair<Integer, Integer>> people) {
         people.sort(SortHeight::comp);
         List<Pair<Integer, Integer>> result = new ArrayList<>(people.size());
         // 这个地方需要初始化，就很坑爹
@@ -41,38 +25,22 @@ public class SortHeight {
         for (int i = 0; i < people.size(); i++) {
             System.out.println(result);
             Pair<Integer, Integer> pair = people.get(i);
-            processPairIndex1(people.get(i), pair.getValue(), result);
+            processPairIndex(people.get(i), pair.getValue(), result);
         }
         return result;
     }
 
-    private static void processPairIndex(Pair<Integer, Integer> pair, int index, Map<Integer, Pair<Integer, Integer>> tmpMap) {
-        Pair<Integer, Integer> oldPair = tmpMap.get(index);
-        if (Objects.isNull(oldPair)) {
-            tmpMap.put(index, pair);
-        } else {
-            // 此处插入排序的判断条件很重要
-            if (oldPair.getValue() > pair.getValue() || oldPair.getKey() > pair.getKey()) {
-                processPairIndex(oldPair, index + 1, tmpMap);
-                tmpMap.put(index, pair);
-            } else {
-                processPairIndex(pair, index + 1, tmpMap);
-            }
-        }
-    }
-
-    private static void processPairIndex1(Pair<Integer, Integer> pair, int index, List<Pair<Integer, Integer>> tmpList) {
+    private static void processPairIndex(Pair<Integer, Integer> pair, int index, List<Pair<Integer, Integer>> tmpList) {
         Pair<Integer, Integer> oldPair = tmpList.get(index);
         if (Objects.isNull(oldPair)) {
             tmpList.set(index, pair);
         } else {
-//            Pair<Integer, Integer> oldPair = tmpList.get(index);
             // 此处插入排序的判断条件很重要
             if (oldPair.getValue() > pair.getValue() || oldPair.getKey() > pair.getKey()) {
-                processPairIndex1(oldPair, index + 1, tmpList);
+                processPairIndex(oldPair, index + 1, tmpList);
                 tmpList.set(index, pair);
             } else {
-                processPairIndex1(pair, index + 1, tmpList);
+                processPairIndex(pair, index + 1, tmpList);
             }
         }
     }
@@ -94,7 +62,7 @@ public class SortHeight {
         param.add(new Pair<>(6, 1));
         param.add(new Pair<>(5, 2));
         System.out.println(param);
-        System.out.println(solution1(param));
+        System.out.println(solution(param));
     }
 
 }
