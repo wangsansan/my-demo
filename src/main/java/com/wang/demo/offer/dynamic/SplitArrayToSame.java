f88de7f6a3d97a237320ea0007bbeb59cdf038f3
wangchunsheng <wangchunsheng04@meituan.com>
2022-09-03 09:16:11 +0800

    add：判断数组是否可以拆分成两个合相同的数组

---
+++ src/main/java/com/wang/demo/offer/dynamic/SplitArrayToSame.java
@@ -0,0 +1,44 @@
+package com.wang.demo.offer.dynamic;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Wangchunsheng
+ * @Date: 2022/9/3 9:03 上午
+ */
+public class SplitArrayToSame {
+    /**
+     * 这道题目用01背包思想分析的话
+     * 原本dp[i][j]是指在重量为j的背包中从0到i个物品中选取物品，求最大价值
+     * 本题中由于价值和重量合二为一了：
+     * dp[i][j]可以理解为在重量为j的背包中从0到i个物品中选取物品，最大重量是否为j，也就是背包是否能正好放满
+     */
+    public static boolean solution(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+
+        int sum = Arrays.stream(nums)
+                .reduce(0, Integer::sum);
+        if (sum % 2 == 1) {
+            return false;
+        }
+
+        int target = sum / 2 ;
+        int[] dp = new int[target + 1];
+        for (int i = 0; i < nums.length; i++) {
+            // 此处01背包问题，为什么j的取值最大是target
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+
+        return dp[target] ==  target;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 11, 5};
+        System.out.println(solution(nums));
+    }
+
+}
