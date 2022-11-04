c326456056a40a4555c62ef7544fa230dbd5d7a0
wangchunsheng <wangchunsheng04@meituan.com>
2022-11-04 08:45:38 +0800

    update:修改题目注释

--- src/main/java/com/wang/demo/offer/others/KeyAndRoom.java
+++ src/main/java/com/wang/demo/offer/others/KeyAndRoom.java
@@ -8,6 +8,15 @@ import java.util.Set;
  * @Author: Wangchunsheng
  * @Date: 2022/11/4 8:19 上午
  */
+
+/**
+ * 841.钥匙和房间
+ * 有 N 个房间，开始时你位于 0 号房间。每个房间有不同的号码：0，1，2，...，N-1，并且房间里可能有一些钥匙能使你进入下一个房间。
+ * 在形式上，对于每个房间 i 都有一个钥匙列表 rooms[i]，每个钥匙 rooms[i][j] 由 [0,1，...，N-1] 中的一个整数表示，其中 N = rooms.length。 钥匙 rooms[i][j] = v 可以打开编号为 v 的房间。
+ * 最初，除 0 号房间外的其余所有房间都被锁住。
+ * 你可以自由地在房间之间来回走动。
+ * 如果能进入每个房间返回 true，否则返回 false。
+ */
 public class KeyAndRoom {
 
     private static Set<Integer> path = Sets.newHashSet();
